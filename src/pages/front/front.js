c1243911ce92ccf5b7e4d68d9d3024ea7c97511a
Сергей Абрамов <abramowsergey1992@gmail.com>
2022-11-28 10:54:14 +0500

    fix

--- src/pages/front/front.js
+++ src/pages/front/front.js
@@ -1,12 +1,9 @@
 var vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty("--vh", `${vh}px`);
 var width = window.innerWidth;
-window.addEventListener("resize", () => {
-	if (width != window.innerWidth) {
-		var vh = window.innerHeight * 0.01;
-		document.documentElement.style.setProperty("--vh", `${vh}px`);
-		width = window.innerWidth;
-	}
+window.addEventListener("orientationchange", () => {
+	var vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
 
 $(function () {
