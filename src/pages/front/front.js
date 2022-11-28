616e1d31757a36131c88fcd5ce185d99533de05d
Сергей Абрамов <abramowsergey1992@gmail.com>
2022-11-28 10:11:12 +0500

    vh

--- src/pages/front/front.js
+++ src/pages/front/front.js
@@ -1,3 +1,14 @@
+var vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+var width = window.innerWidth;
+window.addEventListener("resize", () => {
+	if (width != window.innerWidth) {
+		var vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty("--vh", `${vh}px`);
+		width = window.innerWidth;
+	}
+});
+
 $(function () {
 	let user;
 	if (Cookies.get("user")) {
